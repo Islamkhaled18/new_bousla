8f733eb85d1c80230cfec0ee9740c0c00b3ede87
islam <islam.hegazy72@gmail.com>
2026-01-21 17:02:45 +0200

    about_us and terms conditions

---
+++ Modules/Clients/app/Transformers/TermConditionResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Clients\app\Transformers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TermConditionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+        ];
+    }
+}
---
+++ app/Http/Controllers/Api/AboutUsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AboutUsResource;
+use App\Models\AboutUs;
+use Illuminate\Http\Request;
+
+class AboutUsController extends Controller
+{
+    public function index()
+    {
+         $about_us = AboutUs::get();
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'about_us' =>  AboutUsResource::collection($about_us),
+            ]
+        ], 201);
+    }
+}
